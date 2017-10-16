3a5e762a150f1599ce5b0a604bdd933d36e4e841
André Cordeiro <ahdncordeiro@gmail.com>
2017-10-15 23:29:58 -0300

    Avoiding duplicate

--- youtube-like-keyboard-shortcut.js
+++ youtube-like-keyboard-shortcut.js
@@ -1,3 +1,4 @@
+var eventListenerAdded;
 // define a handler
 function doc_keyUp(e) {
     // this would test if keyCode 85 (u) or keyCode 73 (i) was pressed
@@ -11,8 +12,8 @@ function doc_keyUp(e) {
 }
 
 // register the handler 
-let eventListenerAdded = (eventListenerAdded == null ? false : eventListenerAdded);
-if (!eventListenerAdded) {
+if ((eventListenerAdded == null ? false : eventListenerAdded)) {
+    eventListenerAdded = true;
     console.log("Adding YouTube like keyboard shortcut event listener...")
     document.addEventListener('keyup', doc_keyUp, false);
 }
