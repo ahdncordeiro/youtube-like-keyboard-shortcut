107414c9d89bc3e3e2344a5e035be6bae9d6a4cc
André Cordeiro <ahdncordeiro@gmail.com>
2017-11-21 07:31:52 -0300

    Change to [ and ]

--- youtube-like-keyboard-shortcut.js
+++ youtube-like-keyboard-shortcut.js
@@ -1,11 +1,11 @@
 var shouldAddEventListener;
 // define a handler
 function doc_keyUp(e) {
-    // this would test if keyCode 85 (u) or keyCode 73 (i) was pressed
-    if (e.keyCode == 85) {
+    // this would test if keyCode 219 ([) or keyCode 221 (]) was pressed
+    if (e.keyCode == 219) {
       console.log("Keyboard for like triggered.");
       document.querySelector("#top-level-buttons > ytd-toggle-button-renderer:nth-child(1) > a").click();
-    } else if (e.keyCode == 73) {
+    } else if (e.keyCode == 221) {
       console.log("Keyboard for dislike triggered.");
       document.querySelector("#top-level-buttons > ytd-toggle-button-renderer:nth-child(2) > a").click();      
     }
